e40340b7f5250e1d0da25a0bdf75c8eb35499f80
Jeff <jeff.l.watkins2@gmail.com>
2019-10-01 13:56:18 -0500

    Attempting to add Youtube API key to project

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -1,7 +1,27 @@
 const Videos = Backbone.Collection.extend({
 
   model: Video,
-  // url: src/data/exampleVideoData.js
+  url: 'http://www.googleapis.com/youtube/v3/search',
+  //Google's youtube API url
+
+
+  search: function(query) { //this is a fetch request that conforms to Postman
+    //syntax
+    this.fetch({
+      data: {
+        part: 'snippet',
+        key: window.YOUTUBE_API_KEY,
+        q: query,
+        maxResults: 5,
+        type: 'video',
+        videoEmbeddable: 'true'
+      }
+    })
+  },
+
+  parse: function(response) {
+    return response.items;
+  }
   
   
 });
