7ab9c4a04790c886a1b8f3ba84625b3b151ef250
Jeff <jeff.l.watkins2@gmail.com>
2019-10-01 14:30:57 -0500

    Refactored Search.js

--- src/views/search.js
+++ src/views/search.js
@@ -1,5 +1,24 @@
 const SearchView = Backbone.View.extend({
+  //Event handler for Search
+  events: {
+    'click button': 'performSearch', //Call performSearch if clicked
+    'keyup input': 'handleKeyUp' //Call handle keyup if enter is pressed
+  },
 
+
+  //If enter(keyCode 13) is pressed, perform search
+  handleKeyUp: function(event) {
+    if (event.keyCode === 13) {
+      this.performSearch();
+    }
+  },
+  //Search function
+  performSearch: function() {
+    //Searches collection for a specific input
+    this.collection.search(this.$('input').val().trim());
+    this.$('input').val('');
+  },
+  //Render search view
   render: function() {
     this.$el.html(this.template());
     return this;
