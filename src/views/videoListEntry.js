98b7b8d8779f4f51e6a9f911e7ebacfa63396742
Jeff <jeff.l.watkins2@gmail.com>
2019-10-01 14:43:40 -0500

    VideoListEntry refactor

--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,17 +1,17 @@
 const VideoListEntryView = Backbone.View.extend({
+  //Sets up entry title clickability
+  events: {
+    'click .video-list-entry-title': 'handeClick'
 
+  },
+  //If clicked, run select
+  handleClick: function() {
+    this.model.select();
+  },
+  //Render videoListEntry view on page
   render: function() {
     this.$el.html(this.template(this.model.attributes));
     return this;
-  },
-
-  //make a click event handler that will call a selecting function
-  events: {
-    'click .video-list-entry-title': 'callSelect'
-  },
-  //Create a function that selects the model that is clicked
-  callSelect: function() {
-    this.model.select();
   },
 
   template: templateURL('src/templates/videoListEntry.html'),
