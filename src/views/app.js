63e625dc7fbfaef42251b08134feb4e7022ac7a1
Jeff <jeff.l.watkins2@gmail.com>
2019-10-01 14:23:04 -0500

    Refactored App.js so that the view inititializations are inside app.js' render function

--- src/views/app.js
+++ src/views/app.js
@@ -2,41 +2,47 @@ const AppView = Backbone.View.extend({
 
   el: '#app',
 
+  //Initializes and renders the app
   initialize: function() {
-    this.videos = new Videos(window.exampleVideoData);
+    this.videos = new Videos();
+    //Tells Backbone to listen for sync on videos and call firstSearch 
+    this.listenTo(this.videos, 'sync', this.firstSearch);
+    //Default search in this.videos.search() below
+    this.videos.search('Cute huskies');
     this.render();
-    this.renderVideoList();
-    this.renderVideoPlayer();
-    this.renderSearchView();
   },
 
+  firstSearch: function() {
+    //Selects first video 
+    this.videos.at(0).select();
+  },
 
   render: function() {
-    this.$el.html(this.template());
-    //return new VideoListEntryView({model: video}).render();
+    this.$el.empty();
+    this.$el.html(this.template);
+    
+    //Render player view
+    new VideoPlayerView({
+      model: this.videos.at(0),
+      collection: this.videos,
+      el: this.$('.player')
+    }).render();
+    
+    //Render list view
+    new VideoListView({
+      collection: this.videos,
+      el: this.$('.list')
+    }).render();
+  
+    //Render search view
+    new SearchView({
+      collection: this.videos,
+      el: this.$('.search')
+    }).render();
+  
     return this;
   },
 
-  renderVideoList: function() {
-    new VideoListView({
-      el: this.$('.list'),
-      collection: this.videos
-    }).render();
-  }, 
-
-  renderVideoPlayer: function() {
-    new VideoPlayerView({
-      el: this.$('.player'),
-      collection: this.videos
-    }).render();
-  },
-
-  renderSearchView: function() {
-    new SearchView({
-      el: this.$('.search')
-    }).render();
-  },
-  
   template: templateURL('src/templates/app.html')
 });
 
